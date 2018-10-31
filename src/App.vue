c08a95e11ea06f0475acaf2f9d4b7ecd047f004a
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-31 08:37:55 -0400

    Start Repo create a component folder with header.vue file

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <encabezado></encabezado>
+    <div class="jumbotron">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Encabezadox from './components/encabezado';
+
+export default {
+  components:{
+    encabezado: Encabezadox
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/encabezado.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="header clearfix">
+    <nav >
+      <ul class="nav nav-pills pull-right">
+        <li><a href="">Inicio</a></li>
+        <li><a href="">Usuarios</a></li>
+      </ul>
+      <h3 class="text muted">Aplicacion Usuarios</h3>
+    </nav>
+  </div>
+</template>
