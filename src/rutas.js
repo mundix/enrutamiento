219120c14403cf82140992384aa4123aee3ed989
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-31 08:58:21 -0400

    ...

--- src/rutas.js
+++ src/rutas.js
@@ -1,7 +1,5 @@
 import InicioComponent from './components/inicio.vue';
 
-//como queremos usarlo en otgro archivo que no sea este , lo exportamos
 export const rutas = [
-  { path: '',component: InicioComponent} //cuadno alguien escriba /usuarios vamos a utilizar ciertos componentes
-  //Hay qye crear InicioComponent
+  { path: '',component: InicioComponent}
 ];
